2a874e4a025dc466649dad6dfafa9024654a90e6
XiongHui Guo <gz_shines@msn.com>
2016-05-06 13:42:20 +0800

    Changes: Add packet_size and thread_cnt parameters for shell script.
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./asio_echo_serv 127.0.0.1 8090 64 2
+packet_size=$1
+thread_cnt=$2
+
+packet_size=${packet_size:-64}
+thread_cnt=${thread_cnt:-2}
+
+./asio_echo_serv 127.0.0.1 8090 $packet_size $thread_cnt
 
