018cad72ddf0849d7c3d76d94c557bf2097cb7ba
XiongHui Guo <gz_shines@msn.com>
2016-05-06 16:35:28 +0800

    Added two shell script.
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

---
+++ start_ip_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+client_num=$1
+client_num=${client_num:-10}
+
+packet_size=$2
+packet_size=${packet_size:-64}
+
+echo "client_num = $client_num"
+echo "packet_size = $packet_size"
+
+for ((i=0; i<$client_num; i++));
+do
+    ./asio_echo_client 192.168.2.154 8090 $packet_size &
+done
