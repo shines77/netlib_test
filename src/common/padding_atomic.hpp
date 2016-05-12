97f14e24b6e4c51f256f626c767ff58a4a580388
shines77 <gz_shines@msn.com>
2016-05-12 15:34:28 +0800

    改进了一下padding_atomic()系列类.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/common/padding_atomic.hpp
+++ src/common/padding_atomic.hpp
@@ -11,129 +11,168 @@
 
 namespace asio_test {
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+namespace detail {
+
+//
+// See: http://en.cppreference.com/w/cpp/experimental/type_trait_variable_templates
+// See: http://en.cppreference.com/w/cpp/types/is_arithmetic
+// See: http://en.cppreference.com/w/cpp/types/is_class
+//
+template <typename T, bool isArithmetic = false>
+struct is_inheritable : std::integral_constant<bool,
+                        std::is_class<T>::value  &&
+                        !std::is_final<T>::value &&
+                        !std::is_volatile<T>::value> {};
+
+} // namespace detail
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 struct base_padding_data {};
 
 template <typename T>
-class is_padding_data
+struct is_padding_data
 {
     enum { value = std::is_base_of<T, base_padding_data>::value };
 };
 
-template <typename T>
-struct alignas(CACHELINE_SIZE)
-    padding_data : public std::atomic<T>, public base_padding_data
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize, bool isInheritable = true>
+struct alignas(CacheLineSize)
+padding_data_impl : public T, public base_padding_data
+{
+    typedef T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingSize =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // Cacheline padding
+    char padding[kPaddingSize];
+
+    padding_data_impl(value_type value) : T(value) {};
+    ~padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize>
+struct padding_data_impl<T, CacheLineSize, false> : public base_padding_data
 {
     typedef typename std::remove_volatile<T>::type _T;
     typedef _T value_type;
 
+    static const std::size_t kCacheLineSize = CacheLineSize;
     static const std::size_t kSizeofData = sizeof(value_type);
     static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // T aligned to cacheline size
+    alignas(CacheLineSize) value_type data;
 
     // Cacheline padding
     char padding[kPaddingSize];
 
-    padding_data(_T val) : std::atomic<_T>(val) {};
-    ~padding_data() {};
+    padding_data_impl(value_type value) : data(value) {};
+    ~padding_data_impl() {};
 };
 
-template <typename T>
-struct alignas(CACHELINE_SIZE)
-    padding_volatile : public std::atomic<T>, public base_padding_data
+template <typename T, std::size_t CacheLineSize = CACHELINE_SIZE>
+struct padding_data : public padding_data_impl<
+                                T, CacheLineSize, detail::is_inheritable<T>::value> {};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize, bool isInheritable = true>
+struct alignas(CacheLineSize)
+volatile_padding_data_impl : public T, public base_padding_data
 {
     typedef typename std::remove_volatile<T>::type _T;
     typedef volatile _T value_type;
 
+    static const std::size_t kCacheLineSize = CacheLineSize;
     static const std::size_t kSizeofData = sizeof(value_type);
     static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
 
     // Cacheline padding
     char padding[kPaddingSize];
 
-    padding_volatile(_T val) : std::atomic<_T>(val) {};
-    ~padding_volatile() {};
+    volatile_padding_data_impl(_T value) : T(value) {};
+    ~volatile_padding_data_impl() {};
 };
 
-template <typename T>
-struct alignas(CACHELINE_SIZE)
-    padding_atomic : public std::atomic<T>, public base_padding_data
+template <typename T, std::size_t CacheLineSize>
+struct volatile_padding_data_impl<T, CacheLineSize, false> : public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef volatile _T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingSize =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // (volatile T) aligned to cacheline size
+    alignas(CacheLineSize) value_type data;
+
+    // Cacheline padding
+    char padding[kPaddingSize];
+
+    volatile_padding_data_impl(value_type value) : data(value) {};
+    ~volatile_padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize = CACHELINE_SIZE>
+struct volatile_padding_data : public volatile_padding_data_impl<
+                                        T, CacheLineSize, detail::is_inheritable<T>::value> {};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize = CACHELINE_SIZE>
+struct alignas(CacheLineSize)
+padding_atomic : public std::atomic<typename std::remove_volatile<T>::type>,
+                 public base_padding_data
 {
     typedef typename std::remove_volatile<T>::type _T;
     typedef std::atomic<_T> value_type;
 
+    static const std::size_t kCacheLineSize = CacheLineSize;
     static const std::size_t kSizeofData = sizeof(value_type);
     static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
 
     // Cacheline padding
     char padding[kPaddingSize];
 
-    padding_atomic(_T val) : std::atomic<_T>(val) {};
+    padding_atomic(_T val) : std::atomic<typename std::remove_volatile<T>::type>(val) {};
     ~padding_atomic() {};
 };
 
-/*
-template <typename T>
-struct padding_data : public base_padding_data
-{
-    typedef typename std::remove_volatile<T>::type _T;
-    typedef _T value_type;
-
-    static const std::size_t kSizeofData = sizeof(value_type);
-    static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
-
-    // T aligned to cacheline size
-    alignas(CACHELINE_SIZE) value_type value;
-
-    // Cacheline padding
-    char padding[kPaddingSize];
-
-    void padding_data(_T val) : value(val) {}
-    void ~padding_data(_T val) {}
-};
-
-template <typename T>
-struct padding_volatile : public base_padding_data
-{
-    typedef typename std::remove_volatile<T>::type _T;
-    typedef volatile _T value_type;
-
-    static const std::size_t kSizeofData = sizeof(value_type);
-    static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
-
-    // (volatile T) aligned to cacheline size
-    alignas(CACHELINE_SIZE) value_type value;
-
-    // Cacheline padding
-    char padding[kPaddingSize];
-
-    void padding_volatile(_T val) : value(val) {}
-    void ~padding_volatile(_T val) {}
-};
-
-template <typename T>
-struct padding_atomic : public base_padding_data
+template <typename T, std::size_t CacheLineSize = CACHELINE_SIZE>
+struct padding_atomic_wrapper : public base_padding_data
 {
     typedef typename std::remove_volatile<T>::type _T;
     typedef std::atomic<_T> value_type;
 
+    static const std::size_t kCacheLineSize = CacheLineSize;
     static const std::size_t kSizeofData = sizeof(value_type);
     static const std::size_t kPaddingSize =
-        (CACHELINE_SIZE - kSizeofData) > 0 ? (CACHELINE_SIZE - kSizeofData) : 1;
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
 
     // std::atomic<T> aligned to cacheline size
-    alignas(CACHELINE_SIZE) value_type value;
+    alignas(CacheLineSize) value_type data;
 
     // Cacheline padding
     char padding[kPaddingSize];
 
-    void padding_atomic(_T val) : value(val) {}
-    void ~padding_atomic(_T val) {}
+    padding_atomic_wrapper(_T value) : data(value) {};
+    ~padding_atomic_wrapper() {};
 };
-//*/
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 } // namespace asio_test
 
