df4d292819b9368432f82aad481038a0a31f4051
shines77 <gz_shines@msn.com>
2016-08-30 23:19:51 +0800

    Changes: Added a http-server test shell script;

---
+++ start_local_http_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+test_method=$1
+packet_size=$2
+thread_num=$3
+
+test_method=${test_method:-pingpong}
+packet_size=${packet_size:-64}
+thread_num=${thread_num:-1}
+
+echo "test_method = $test_method"
+
+./asio_echo_serv --host=127.0.0.1 --port=8090 --mode=http --test=$test_method --packet-size=$packet_size --thread-num=$thread_num
