82cf9fb51354ae079da8a59e88e7d8b6f2913173
shines77 <gz_shines@msn.com>
2016-08-31 01:38:03 +0800

    Changes: update the windows x64 test bat files;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- bin/vc2015/x64-Release/start_client.bat
+++ bin/vc2015/x64-Release/start_client.bat
@@ -1,2 +1,2 @@
 @echo "start test clients ..."
-for /l %%i in (1,1,10) do start /min "asio_echo_client" asio_echo_client.exe 192.168.2.154 8090 64
+for /l %%i in (1,1,10) do start /min "asio_echo_client" asio_echo_client.exe --host=192.168.88.102 --port=8090 --mode=echo --test=qps --packet-size=64 --thread-num=1
