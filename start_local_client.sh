73fb947627cfeb3eb3c44c7e9f55ccf801b63bbc
shines77 <gz_shines@msn.com>
2016-08-30 12:08:01 +0800

    Changes: The test shell script add test_method parameter;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- start_local_client.sh
+++ start_local_client.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-client_num=$1
-packet_size=$2
+test_method=$1
+client_num=$2
+packet_size=$3
 
+test_method=${test_method:-pingpong}
 client_num=${client_num:-10}
 packet_size=${packet_size:-64}
 
+# echo "test_method = $test_method"
 # echo "client_num = $client_num"
 # echo "packet_size = $packet_size"
 
 for ((i=0; i<$client_num; i++));
 do
-    ./asio_echo_client --host=127.0.0.1 --port=8090 --mode=echo --test=qps --packet-size=$packet_size &
+    ./asio_echo_client --host=127.0.0.1 --port=8090 --mode=echo --test=$test_method --packet-size=$packet_size &
 done
--- start_local_server.sh
+++ start_local_server.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-packet_size=$1
-thread_cnt=$2
+test_method=$1
+packet_size=$2
+thread_cnt=$3
 
+test_method=${test_method:-pingpong}
 packet_size=${packet_size:-64}
 thread_cnt=${thread_cnt:-0}
 
-./asio_echo_serv --host=127.0.0.1 --port=8090 --mode=echo --test=qps --packet-size=$packet_size --thread-num=$thread_cnt
+echo "test_method = $test_method"
+
+./asio_echo_serv --host=127.0.0.1 --port=8090 --mode=echo --test=$test_method --packet-size=$packet_size --thread-num=$thread_cnt
