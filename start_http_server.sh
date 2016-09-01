652e9d9a8aa2bec0cd7f30a11c8a3683b6c443d5
shines77 <gz_shines@msn.com>
2016-09-01 15:21:08 +0800

    Changes: Added nodelay args for hhtp_server test shell script;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- start_http_server.sh
+++ start_http_server.sh
@@ -3,11 +3,13 @@
 test_method=$1
 packet_size=$2
 thread_num=$3
+nodelay=$4
 
 test_method=${test_method:-pingpong}
 packet_size=${packet_size:-64}
 thread_num=${thread_num:-1}
+nodelay=${nodelay:-0}
 
 echo "test_method = $test_method"
 
-./asio_echo_serv --host=0.0.0.0 --port=8090 --mode=http --test=$test_method --packet-size=$packet_size --thread-num=$thread_num
+./asio_echo_serv --host=0.0.0.0 --port=8090 --mode=http --test=$test_method --packet-size=$packet_size --thread-num=$thread_num --nodelay=$nodelay
